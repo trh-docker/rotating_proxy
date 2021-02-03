76c139506f898c38b514a83316cdeef7b02c6d13
spivegin <spivegin@txtsme.com>
2021-02-03 12:00:56 -0500

    fixed missing "" in bash script

--- files/bash/entry.sh
+++ files/bash/entry.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 password=${PROXY_PASSWORD}
 domain=${PROXY_DOMAIN}
+
 tor &
 brook socks5tohttp -s 127.0.0.1:9050 -l 127.0.0.1:8010 &
 brook socks5tohttp -s 127.0.0.1:9050 -l 127.0.0.1:8011 &
 brook socks5tohttp -s 127.0.0.1:9050 -l 127.0.0.1:8012 &
+gobetween  from-file -f json /root/gobetween.json &
+echo $PROXY_PASSWORD
+echo $PROXY_DOMAIN
 
-if [ $domain == "" ]
+if [ "$domain" == "" ];
 then
- brook server -l 0.0.0.0:8080 -p $password &
-else
- brook wsserver --domain $domain  -p $password &
+   brook server -l 0.0.0.0:8080 -p $password 
 fi
-
-gobetween  from-file -f json /root/gobetween.json
+if [ "$domain" == "" ];
+then
+   brook wsserver --domain $domain  -p $password 
+fi
+ 
